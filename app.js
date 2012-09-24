f85a4e9e8fe638e1cecd2282baf97cd8b9eea1b0
nickxbs <febbrari@gmail.com>
2012-09-25 01:19:16 +0200

    indtroduzione mongo

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-var express = require('express');
-var app = express();
-//var app = express.createServer(express.logger());
-app.set('title', 'MiaApplication');
-// development only
-//if ('development' == app.get('env')) {
-//  app.set('db uri', 'localhost/dev');
-//}
-//
-//// production only
-//if ('production' == app.get('env')) {
-//  app.set('db uri', 'n.n.n.n/prod');
-//}                                  });
+var express = require('express'),
+    models = require('./model'),
+    db,
+    Comune,
+	mongoose = require('mongoose');
 
+var app = express();
+app.set('title', 'MiaApplication');
+
+								 
+if ('development' == app.get('env')) {
+	app.set('db-uri', 'mongodb://localhost/nodefordummies');
+	 app.use(express.errorHandler({ dumpExceptions: true }));
+  app.set('view options', {
+    pretty: true
+  });
+}
+
+// production only
+if ('production' == app.get('env')) {
+	app.set('db-uri', 'mongodb://localhost/nodefordummies');
+} 
+
+models.defineModels(mongoose,function() {
+  app.Comune = Comune = mongoose.model('Comune');
+  db = mongoose.connect(app.set('db-uri'));
+})
 
 app.get('/', function(req, res){
                   res.send('Hello World');
@@ -26,7 +39,21 @@ app.get('/prova', function(req, res){
                   }
    
                   });
-
+				  
+app.get('/comuni', function(req, res) {
+  Comune.find({descrizione: "Castenedolo" } ,
+  function(err, comuni) {
+    comuni = comuni.map(function(d) {
+		return { 
+		descrizione: d.descrizione, 
+		id: d._id,
+		provincia: d.provincia	};
+		});
+    res.send(comuni);
+  });
+});
+				  
+				  
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
