ed919bd180330ebba5bb205ab009c2c77b4594dc
nickxbs <febbrari@gmail.com>
2012-09-27 09:36:36 +0200

    ricerca con like su descrizione comune

--- app.js
+++ app.js
@@ -2,80 +2,91 @@ var express = require('express'),
     models = require('./model'),
     db,
     Comune,
-	mongoose = require('mongoose'),
-	jade=require('jade');
+	mongoose = require('mongoose');
 
 var app = express();
 app.set('title', 'MiaApplication');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/../public'));
 app.use(express.bodyParser());
-								 
+
 if ('development' == app.get('env')) {
-	app.set('db-uri', 'mongodb://localhost/nodefordummies');
-	app.use(express.errorHandler({ dumpExceptions: true }));
-	app.set('view options', {
-    pretty: true
-  });
+    app.set('db-uri', 'mongodb://localhost/nodefordummies');
+    app.use(express.errorHandler({ dumpExceptions: true }));
+    app.set('view options', {
+        pretty: true
+    });
 }
 
 // production only
 if ('production' == app.get('env')) {
-	app.set('db-uri', 'mongodb://heroku:studiofarma@alex.mongohq.com:10017/app7875330');
-} 
+    app.set('db-uri', 'mongodb://heroku:studiofarma@alex.mongohq.com:10017/app7875330');
+}
 
-
-models.defineModels(mongoose,function() {
-  app.Comune = Comune = mongoose.model('Comune');
-  db = mongoose.connect(app.set('db-uri'));
+//Initialize models
+models.defineModels(mongoose, function () {
+    app.Comune = Comune = mongoose.model('Comune');
+    db = mongoose.connect(app.set('db-uri'));
 })
 
-app.get('/', function(req, res){
-                  res.send('Hello World '+app.get('env'));
-                  });
-app.post('/insert', function(req, res){
-                console.log(req.body);
-                var result= req.body.txtProvincia;
-                console.log('Risultato: '+result);
-				var newComune= new Comune({
-				descrizione:req.body.txtDescrizione,
-				provincia:req.body.txtProvincia
-				})
-				newComune.save(function(err) {
-				if(err)
-					console.log(err);				
-				res.send('Comune inserito');
-				}); 
-            });
-app.get('/prova', function(req, res){
-                
-                if(req.is('text/html')){
-                  res.send('<html><head><title>'+app.get('title')+'</title></head>Hello HTML</html>');
-                  }
-                 if(req.is('json')){
-                  res.send('<html><head><title>'+app.get('title')+'</title></head>Hello JSON</html>');
-                  }
-   
-                  });
-				  
-app.get('/comuni', function(req, res) {
-  Comune.find({} ,
-  function(err, comuni) {
-    comuni = comuni.map(function(d) {
-		return { 
-		descrizione: d.descrizione, 
-		id: d._id,
-		provincia: d.provincia	};
-		});
-    res.send(comuni);
-  });
+//Hello World 
+app.get('/', function (req, res) {
+    res.send('Hello World in' + app.get('env'));
 });
-app.get('/InsertComune', function(req, res){
-	res.render('index.jade', {title: 'Scaliamo di bestia'});
+app.get('/prova', function (req, res) {
+    if (req.is('text/html')) {
+        res.send('<html><head><title>' + app.get('title') + '</title></head>Hello HTML</html>');
+    }
+    if (req.is('json')) {
+        res.send('<html><head><title>' + app.get('title') + '</title></head>Hello JSON</html>');
+    }
 });
-				  
-				  
+
+
+app.post('/comuni/add', function (req, res) {
+    var newComune = new Comune({
+        descrizione: req.body.txtDescrizione,
+        provincia: req.body.txtProvincia
+    })
+    newComune.save(function (err) {
+        if (err)
+            console.log(err);
+        res.send('Comune inserito');
+    });
+});
+app.get('/comuni/add/:descrizione/:provincia', function (req, res) {
+    var newComune = new Comune({
+        descrizione: req.params.descrizione,
+        provincia: req.params.provincia
+    })
+    newComune.save(function (err) {
+        if (err)
+            console.log(err);
+        res.send('Comune inserito');
+    });
+});
+
+
+
+app.get('/comuni/:descrizione', function (req, res) {
+    Comune.find({ descrizione: { $regex: '\w*'+req.params.descrizione+'\w*', $options: 'i' } },
+    function (err, comuni) {
+        comuni = comuni.map(function (d) {
+            return {
+                descrizione: d.descrizione,
+                id: d._id,
+                provincia: d.provincia
+            };
+        });
+        res.send(comuni);
+    });
+});
+app.get('/comuni/add', function (req, res) {
+    res.render('index.jade', { title: 'Scaliamo di bestia' });
+});
+
+
 var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function () {
+    console.log("Listening on " + port);
 });
