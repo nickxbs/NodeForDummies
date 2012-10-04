5409eca05b93493ee614bab4db5c22bf3d96adea
Bruno <bruno@Br1-Xubuntu.(none)>
2012-10-04 23:25:47 +0200

    Socket.IO a go go

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var express = require('express'),
+http = require('http'),    
     models = require('./model'),
     db,
     Comune,
-	mongoose = require('mongoose');
+mongoose = require('mongoose');
 
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
 app.set('title', 'MiaApplication');
 app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/../public'));
+app.use('public', express.static(__dirname + '/public'));
 app.use(express.bodyParser());
 
 if ('development' == app.get('env')) {
@@ -30,10 +34,41 @@ models.defineModels(mongoose, function () {
     db = mongoose.connect(app.set('db-uri'));
 })
 
+//Socket.IO
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+
+  socket.on('logon', function (data) {
+    console.log(data);
+      console.log('Adesso dormo un po');
+      var start = Date.now();
+      setTimeout(function(){ console.log(Date.now() - start);
+			     if(data.name === 'bruno'){
+				 socket.emit('login', 'Ciao Bruno');
+			     }
+			     else{
+				 socket.emit('login', 'Accesso non consentito')
+			     }
+
+ }, 5000);
+      
+  });
+});
+
+
 //Hello World 
 app.get('/', function (req, res) {
     res.send('Hello World in' + app.get('env'));
 });
+
+app.get('/public', function (req, res) {
+  res.sendfile(__dirname + '/public/index.html');
+});
+
 app.get('/prova', function (req, res) {
     if (req.is('text/html')) {
         res.send('<html><head><title>' + app.get('title') + '</title></head>Hello HTML</html>');
@@ -108,6 +143,6 @@ app.get('/comuni/:descrizione', function (req, res) {
 
 
 var port = process.env.PORT || 3000;
-app.listen(port, function () {
+server.listen(port, function () {
     console.log("Listening on " + port);
 });
