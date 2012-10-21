71375f9f826918df87e7e56075442fa1aa3f9fed
Federico Rota <federico.rota01@gmail.com>
2012-10-21 15:20:02 +0200

    Riordine variabili e indentazione

--- Concentratore.js
+++ Concentratore.js
@@ -3,31 +3,31 @@ exports.concentratore = (function(sock, mongoose){
                 var warning = 2;
                 var informational = 3;
 
-var Schema = mongoose.Schema;
-var eccezioneSchema = new Schema({
-      'mittente': { type: String, index: true },
-      'messaggio': String
-  });
-var eccezioneModel= mongoose.model('eccezioneModel', eccezioneSchema);
+                var Schema = mongoose.Schema;
+                var eccezioneSchema = new Schema({
+                        'mittente': { type: String, index: true },
+                        'messaggio': String
+                        });
+                var eccezioneModel= mongoose.model('eccezioneModel', eccezioneSchema);
 
                 console.log("Prova concentratore");
                 function handleEccezione(messaggio, mittente){
-                  var newEccezione = new eccezioneModel({
+                var newEccezione = new eccezioneModel({
                         mittente: mittente,
                         messaggio: messaggio
-                    });
-                    newEccezione.save(function (err) {
+                        });
+                newEccezione.save(function (err) {
                         if (err)
-                            console.log(err);                        
+                        console.log(err);                        
                         notificaEccezioneGestita();
-                    });
-                }
-                function notificaEccezioneGestita(){
-					sock.sockets.on('connection', function (s) {
+                        });
+}
+function notificaEccezioneGestita(){
+        sock.sockets.on('connection', function (s) {
                         s.emit('eccezioneGestitaEvent', { data: 'ok' });
-					});
-				}
-                return {
-                getEccezione: handleEccezione
-                };
+                        });
+}
+return {
+getEccezione: handleEccezione
+};
 });
--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var express = require('express'),
-http = require('http'),    
+    http = require('http'),    
     models = require('./model'),
     db,
     Comune,
 mongoose = require('mongoose');
 
 var app = express();
-var server = require('http').createServer(app);
+var server = http.createServer(app);
 var io = require('socket.io').listen(server);
+var port = process.env.PORT || 3000;
 
 if ('development' == app.get('env')) {
     app.set('db-uri', 'mongodb://localhost/nodefordummies');
@@ -22,7 +23,7 @@ if ('production' == app.get('env')) {
     app.set('db-uri', 'mongodb://heroku:studiofarma@alex.mongohq.com:10017/app7875330');
 }
 
-var db = mongoose.connect(app.set('db-uri'));
+mongoose.connect(app.set('db-uri'));
 var concentratore = require('./Concentratore').concentratore(io, mongoose);
 
 app.set('title', 'MiaApplication');
@@ -33,7 +34,6 @@ app.use(express.bodyParser());
 app.get('/eccezione', function (req, res) {
   res.render('index.jade' );
 });
-var port = process.env.PORT || 3000;
 app.get('/eccezione/getAll', function (req, res) {
     console.log("Inizio ricerca...");
     mongoose.model('eccezioneModel').find({},
@@ -47,14 +47,12 @@ app.get('/eccezione/getAll', function (req, res) {
             };
         });
         console.log("Fine ricerca");
-        //res.send(eccezioni);
-		 res.render('eccezione.jade',{
+        res.render('eccezione.jade',{
 		 "eccezioni":eccezioni,
 		 "port":port
 		 } );
     });
     console.log("Fine call");
-
 });
 
 app.post('/eccezione', function(req, res){
