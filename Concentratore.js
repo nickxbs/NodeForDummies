2dcd740ee403c2428ea681f4752e37255d2585cf
Federico Rota <federico.rota01@gmail.com>
2012-10-21 23:31:02 +0200

    Socket.IO - Express integration working

--- Concentratore.js
+++ Concentratore.js
@@ -1,4 +1,4 @@
-exports.concentratore = (function(sock, mongoose){
+exports.concentratore = (function(io, mongoose){
                 var critical = 1;
                 var warning = 2;
                 var informational = 3;
@@ -10,24 +10,17 @@ exports.concentratore = (function(sock, mongoose){
                         });
                 var eccezioneModel= mongoose.model('eccezioneModel', eccezioneSchema);
 
-                console.log("Prova concentratore");
                 function handleEccezione(messaggio, mittente){
                 var newEccezione = new eccezioneModel({
                         mittente: mittente,
                         messaggio: messaggio
                         });
                 newEccezione.save(function (err) {
-                        if (err)
-                        console.log(err);                        
-                        notificaEccezioneGestita();
+                        console.log("conc. sockets" + io.sockets.length);
+                        io.sockets.emit('eccezioneGestitaEvent', { Mittente: mittente, Messaggio: messaggio });
                         });
-}
-function notificaEccezioneGestita(){
-        sock.sockets.on('connection', function (s) {
-                        s.emit('eccezioneGestitaEvent', { data: 'ok' });
-                        });
-}
-return {
-getEccezione: handleEccezione
-};
+                }
+                return {
+                        getEccezione: handleEccezione
+                };
 });
--- app.js
+++ app.js
@@ -3,24 +3,24 @@ var express = require('express'),
     models = require('./model'),
     db,
     Comune,
-mongoose = require('mongoose');
+    mongoose = require('mongoose');
 
 var app = express();
 var server = http.createServer(app);
-var io = require('socket.io').listen(server);
 var port = process.env.PORT || 3000;
+var io = require('socket.io').listen(server);
 
 if ('development' == app.get('env')) {
-    app.set('db-uri', 'mongodb://localhost/nodefordummies');
-    app.use(express.errorHandler({ dumpExceptions: true }));
-    app.set('view options', {
-        pretty: true
-    });
+        app.set('db-uri', 'mongodb://localhost/nodefordummies');
+        app.use(express.errorHandler({ dumpExceptions: true }));
+        app.set('view options', {
+                pretty: true
+                });
 }
 
 // production only
 if ('production' == app.get('env')) {
-    app.set('db-uri', 'mongodb://heroku:studiofarma@alex.mongohq.com:10017/app7875330');
+        app.set('db-uri', 'mongodb://heroku:studiofarma@alex.mongohq.com:10017/app7875330');
 }
 
 mongoose.connect(app.set('db-uri'));
@@ -32,34 +32,53 @@ app.use('public', express.static(__dirname + '/public'));
 app.use(express.bodyParser());
 
 app.get('/eccezione', function (req, res) {
-  res.render('index.jade' );
-});
-app.get('/eccezione/getAll', function (req, res) {
-    console.log("Inizio ricerca...");
-    mongoose.model('eccezioneModel').find({},
-    function (err, eccezioni) {
-        eccezioni = eccezioni.map(function (d) {
-            return {
-                messaggio: d.messaggio,
-                id: d._id,
-                mittente: d.mittente
-				
-            };
-        });
-        console.log("Fine ricerca");
-        res.render('eccezione.jade',{
-		 "eccezioni":eccezioni,
-		 "port":port
-		 } );
-    });
-    console.log("Fine call");
-});
+                res.render('index.jade' );
+                });
 
+app.get('/eccezione/getAll', function (req, res) {
+                console.log("Inizio ricerca...");
+                mongoose.model('eccezioneModel').find({},
+                        function (err, eccezioni) {
+                        eccezioni = eccezioni.map(function (d) {
+                                return {
+                                        messaggio: d.messaggio,
+                                        id: d._id,
+                                        mittente: d.mittente
+                                        };
+                        });
+                        console.log("Fine ricerca");
+                        res.render('eccezione.jade', {
+                                "eccezioni":eccezioni,
+                                "port":port
+                                });
+                        });
+                console.log("Fine call");
+                });
 app.post('/eccezione', function(req, res){
                 concentratore.getEccezione(req.body.messaggio, req.body.mittente);
-				res.send("OK");
+                res.send("OK");
+                });
+
+io.sockets.on('connection', function (socket) {
+                socket.on('eccezioneGestitaEvent', function (data) {
+                        mongoose.model('eccezioneModel').find({},
+                                function (err, eccezioni) {
+                                eccezioni = eccezioni.map(function (d) {
+                                        return {
+                                               messaggio: d.messaggio,
+                                               id: d._id,
+                                               mittente: d.mittente
+                                               };
+                                });
+                                console.log("Refresh eccezione inserita");
+                                res.render('eccezione.jade',{
+                                        "eccezioni":eccezioni,
+                                        "port":port
+                                        } );
+                                });
+                        });
                 });
 
 server.listen(port, function () {
-    console.log("Listening on " + port);
-});
+                console.log("Listening on " + port);
+                });
