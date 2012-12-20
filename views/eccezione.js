cbd9b0582c278cb4afd5306e9d57a826fff38da8
nickxbs <febbrari@gmail.com>
2012-12-20 16:01:49 +0100

    fix render eccezioni

--- views/eccezione.js
+++ views/eccezione.js
@@ -1,5 +1,4 @@
 
 var socket = io.connect(location.origin);
 socket.on('eccezioneGestitaEvent', function (data) {
-		window.location.reload();  
-		});
+$('#tab > tbody:last').append('<tr><td>'+data.Mittente+'</td><td>'+data.Messaggio+'</td></tr>');		});
